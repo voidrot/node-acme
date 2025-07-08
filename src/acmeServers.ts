96cd3d814d6342109d2c252989b6ddf3b7ffd6b3
Buck Brady <bbrady145@gmail.com>
2025-07-07 18:37:51 -0600

    add export to known ACME servers

--- src/acmeServers.ts
+++ src/acmeServers.ts
@@ -1,2 +1,2 @@
-const LETS_ENCRYPT_STAGING = 'https://acme-staging-v02.api.letsencrypt.org/directory'
-const LETS_ENCRYPT_PRODUCTION = 'https://acme-v02.api.letsencrypt.org/directory'
+export const LETS_ENCRYPT_STAGING = 'https://acme-staging-v02.api.letsencrypt.org/directory'
+export const LETS_ENCRYPT_PRODUCTION = 'https://acme-v02.api.letsencrypt.org/directory'
